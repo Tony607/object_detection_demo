7a1eb2db32adc3b89dd5750afbfa54da23a810b2
MrQuang89 <nmquang133@gmail.com>
2020-08-24 15:55:30 +0700

    changed generate tf_recoder file 1

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -24,7 +24,7 @@ from PIL import Image
 from object_detection.utils import dataset_util
 from collections import namedtuple, OrderedDict
 
-flags = tf.app.flags
+flags = tf.compat.v1.app.flags
 flags.DEFINE_string("csv_input", "", "Path to the CSV input")
 flags.DEFINE_string("output_path", "", "Path to output TFRecord")
 flags.DEFINE_string(
