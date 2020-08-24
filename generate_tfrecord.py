d446c1233f946ef846bedb8be0183f1deb759f32
MrQuang89 <nmquang133@gmail.com>
2020-08-24 14:10:07 +0700

    changed in generate_tfrecord.py file

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -50,7 +50,8 @@ def split(df, group):
 
 
 def create_tf_example(group, path, label_map):
-    with tf.gfile.GFile(os.path.join(path, "{}".format(group.filename)), "rb") as fid:
+    #with tf.gfile.GFile(os.path.join(path, "{}".format(group.filename)), "rb") as fid:
+    with tf.io.gfile.GFile(os.path.join(path, "{}".format(group.filename)), "rb") as fid:
         encoded_jpg = fid.read()
     encoded_jpg_io = io.BytesIO(encoded_jpg)
     image = Image.open(encoded_jpg_io)
@@ -104,7 +105,8 @@ def create_tf_example(group, path, label_map):
 
 
 def main(_):
-    writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
+    #writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
+    writer = tf.io.TFRecordWriter(FLAGS.output_path)
     path = os.path.join(os.getcwd(), FLAGS.img_path)
     examples = pd.read_csv(FLAGS.csv_input)
 
@@ -131,4 +133,5 @@ def main(_):
 
 
 if __name__ == "__main__":
-    tf.app.run()
+    #tf.app.run()
+    tf.compat.v1.app.run()
