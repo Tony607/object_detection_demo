42becc53418b35cc74e32cbded5a81a972894840
vanhuytran2610 <62320089+vanhuytran2610@users.noreply.github.com>
2021-04-20 21:41:51 +0700

    Update generate_tfrecord.py

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -15,7 +15,7 @@ from __future__ import absolute_import
 import os
 import io
 import pandas as pd
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 import sys
 
 sys.path.append("../../models/research")
