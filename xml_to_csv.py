8e3475465a261c6caac799024bcda61ad288aaa0
Harrison Cole <harrisoncole05@gmail.com>
2021-05-08 15:01:14 +1000

    Prevent index out of bounds issue

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -33,15 +33,16 @@ def xml_to_csv(path):
         root = tree.getroot()
         for member in root.findall("object"):
             classes_names.append(member[0].text)
+            box = member.find("bndbox")
             value = (
                 root.find("filename").text,
                 int(root.find("size")[0].text),
                 int(root.find("size")[1].text),
                 member[0].text,
-                int(member[4][0].text),
-                int(member[4][1].text),
-                int(member[4][2].text),
-                int(member[4][3].text),
+                int(box[0].text),
+                int(box[1].text),
+                int(box[2].text),
+                int(box[3].text),
             )
             xml_list.append(value)
     column_name = [
